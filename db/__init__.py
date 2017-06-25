7081e9051be37f8841265fb1cdbd07fd19d45380
mgunyho <marci.gunyho@gmail.com>
2017-06-25 15:12:32 +0300

    Remove some sqlite features that are unnecessary with mongodb.

--- db/__init__.py
+++ db/__init__.py
@@ -6,24 +6,11 @@ may be used corresponding to different levels of aggregation. In this case the
 db manager handles aggregation and querying the appropriate database if the 
 query is a range.
 """
-#import sqlite3
 from pymongo import MongoClient
-
-
-"""
-The database schema used.
-"""
-#TODO: add some way to identify calibration parameters
-#TODO: figure out if this should be defined in the configuration instead...
-TABLES = {
-    "data": [("timestamp", "integer primary key"), ("sensor_value", "integer"), ("calibrated_value", "real")],
-    "calibration_parameters": [("timestamp", "integer primary key"), ("values", "text")], #TODO: something like 'parameters_json' ?
-    }
 
 #TODO: does the connection need to be closd manually w/ mongodb?
 """
-A class to handle database queries. Opens a database connection upon creation,
-which needs to be closed manually.
+A class to handle database queries. 
 """
 class DatabaseManager(object):
 
@@ -134,24 +121,10 @@ class DatabaseManager(object):
     c.execute("INSERT (?, ?) INTO TABLE ... ??? ", timestamp, calibration_dict)
     c.commit()
 
-
-  # initialize the tables to the database using self._conn
-  def initialize(self):
-    import json
-    c = self._conn.cursor()
-    try:
-      #schema_string = "{}".format(", ".join(", ".join()))
-      c.execute("""CREATE TABLE ? (?)""", table, schema_string)
-    except sqlite3.OperationalError as e:
-      print("Abort: error when creating table {} in database {}: {}".format(t, self.db_path, e))
-      sefl._conn.rollback()
-      break
-
 # necessary?
 class DBException(Exception):
   #TODO
   pass
-
 
 
 # for testing and manual database management
@@ -165,11 +138,7 @@ if __name__ == "__main__":
       dest = "config_file",
       help = "use CONFIG_FILE as the configuration file instead of the default")
 
-  ap.add_argument("--initialize",
-      dest = "init", action = "store_true",
-      help = "Initialize the database specified in the configuration according to the schema. Don't do anything if the database already exists.")
-
-  #TODO: options for resetting, initializing etc.
+  #TODO: option to reset/dump database
 
   args = ap.parse_args()
 
@@ -177,8 +146,6 @@ if __name__ == "__main__":
 
   dbm = DatabaseManager(cfg)
 
-  if args.init:
-    dbm.initialize()
 
   #TODO: tests...
   #dbm.query_range((0, 100))
