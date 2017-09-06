86620c3ae983025208fbb61aac13a93c483fb4bc
mgunyho <marci.gunyho@gmail.com>
2017-09-06 21:37:27 +0300

    Rewrote calibration script to work interactively.

--- sensor/calibrate.py
+++ sensor/calibrate.py
@@ -11,55 +11,47 @@ import os, sys
 import time
 
 if __name__ == "__main__":
-  os.chdir(os.path.dirname(__file__))
-  FILENAME = "calibration.csv"
-  SEP = "," # separator used for csv
+  import argparse, config
 
-  try:
-    answer = None
-    if os.path.exists(FILENAME):
-      answer = input("Warning: overwrite file '{}' (y/n)? ".format(FILENAME))
-    if answer is not None and "n" in answer:
-      print("Quitting. Not overwriting '{}'.".format(FILENAME))
-      sys.exit()
+  ap = argparse.ArgumentParser()
+  ap.add_argument("-c", "--config",
+      dest = "config_file",
+      help = "use CONFIG_FILE as the configuration file instead of the default")
 
-    elif answer is not None and "y" in answer:
-      print("Overwriting '{}'".format(FILENAME))
+  args = ap.parse_args()
 
-    s = sensor.Sensor()
+  cfg = config.get_config_dict(args.config_file)
 
-    with open(FILENAME, "w") as f:
-      while True:
-        #TODO: make this less of a hack (currently copied from __name__ == __main__ section of sensor/__init)
-        poll_result = s.poll(averaging_time = 0.01, avg_interval = 0.001) #TODO: adjust these timings
-        rawValue = poll_result["rawValue"]
-        timestamp = time.time()
-        txt = SEP.join([str(rawValue), str(timestamp)]) + "\n"
-        f.write(txt)
-        fmt = "{:0=10} {:>12.1f}"
-        sys.stdout.write(fmt.format(rawValue, timestamp) + "\r")
-        sys.stdout.flush()
-        time.sleep(0.05)
+  print("Starting calibration.")
 
-  except KeyboardInterrupt:
-    sensor.driver.cleanup()
+  # how many seconds to average each measurement for
+  AVG_TIME = 10.
 
-  except Exception:
-    sensor.driver.cleanup()
-    raise
+  s = sensor.Sensor(cfg)
 
-  print("\n\nPlotting data if possible...")
-  try:
-    import numpy as np
-    import matplotlib.pyplot as plt
+  instructions = [
+      #("coffee_empty_value", "Remove the decanter from the coffee machine and press enter."),
+      ("coffee_empty_decanter_value", "Place an empty decanter on to the decanter tray and press enter."),
+      ("coffee_full_value", "Now fill up the decanter completely, place it on the tray and press enter."),
+      #("coffee_start_value", "Fill the water tank and press enter."),
+      ]
 
-    data = np.loadtxt(FILENAME, delimiter = SEP)
-
-    plt.plot(data[:, 0], data[:, 1])
-
-    plt.show()
-
-  except ImportError as e:
-    print("{} not found, not plotting.".format(e.name))
+  calibration = {}
 
 
+  for param, msg in instructions:
+    input(msg + "\n")
+    print("Calibrating (averaging for {} seconds)...".format(AVG_TIME))
+    poll_result = s.poll(averaging_time = AVG_TIME, avg_interval = 0.001) #TODO: adjust these timings
+    rawValue = poll_result["rawValue"]
+
+    #sys.stdout.write(fmt.format(rawValue, timestamp) + "\r")
+    #sys.stdout.flush()
+
+    calibration[param] = rawValue
+
+  sensor.driver.cleanup()
+
+  print("Finished. Your calibration parameters are:")
+  for k, v in calibration.items():
+    print("{}: {}".format(k, v))
