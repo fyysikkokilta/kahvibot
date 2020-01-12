395f097ce5e961553d463045e0677601f6e26b39
mgunyho <marci.gunyho@gmail.com>
2020-01-12 09:40:45 +0200

    Fix indentation

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ import syslog
 Default options
 """
 _CONFIG_DEFAULTS = {
-      "telegram" : {
-        "bot_token" : "",
-        "group_trigger_threshold" : 20,
-      },
+            "telegram" : {
+                "bot_token" : "",
+                "group_trigger_threshold" : 20,
+            },
 
-    }
+        }
 
 """
 Initialize a configparser dictionary with given or default filename and
@@ -24,42 +24,42 @@ return it
 """
 def get_config_dict(filename = None):
 
-  if filename is None:
-    cfg_path = path.dirname(__file__)
-    filename = path.join(cfg_path, "config.ini")
+    if filename is None:
+        cfg_path = path.dirname(__file__)
+        filename = path.join(cfg_path, "config.ini")
 
-  cp = configparser.ConfigParser()
+    cp = configparser.ConfigParser()
 
-  # read default values from dict if they are not given in the config file.
-  cp.read_dict(_CONFIG_DEFAULTS)
+    # read default values from dict if they are not given in the config file.
+    cp.read_dict(_CONFIG_DEFAULTS)
 
-  if not path.exists(filename):
-    raise IOError("Configuration file {} does not exist.".format(filename))
+    if not path.exists(filename):
+        raise IOError("Configuration file {} does not exist.".format(filename))
 
-  cp.read(filename)
-  syslog.syslog(syslog.LOG_INFO, "config: Using configuration file " + filename)
+    cp.read(filename)
+    syslog.syslog(syslog.LOG_INFO, "config: Using configuration file " + filename)
 
-  # check for placeholder telegram bot token
-  if set(cp["telegram"]["bot_token"]) == set("X"):
-    cp["telegram"]["bot_token"] = ""
+    # check for placeholder telegram bot token
+    if set(cp["telegram"]["bot_token"]) == set("X"):
+        cp["telegram"]["bot_token"] = ""
 
-  return cp
+    return cp
 
 
 if __name__ == "__main__":
-  import argparse
-  from pprint import pprint
+    import argparse
+    from pprint import pprint
 
-  ap = argparse.ArgumentParser()
-  ap.add_argument("-c", "--config",
-      dest = "config_file",
-      help = "use CONFIG_FILE as the configuration file instead of the default")
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--config",
+            dest = "config_file",
+            help = "use CONFIG_FILE as the configuration file instead of the default")
 
-  args = ap.parse_args()
+    args = ap.parse_args()
 
-  cfg = get_config_dict(args.config_file)
+    cfg = get_config_dict(args.config_file)
 
-  for sec in cfg.sections():
-    print("{}:".format(sec))
-    pprint(list(cfg[sec].items()))
-    print("")
+    for sec in cfg.sections():
+        print("{}:".format(sec))
+        pprint(list(cfg[sec].items()))
+        print("")
