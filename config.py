71c60526d7b9eef123d057744be160fc41a0330e
mgunyho <marci.gunyho@gmail.com>
2020-01-12 09:36:48 +0200

    Clean up config module and default config file

--- config.py
+++ config.py
@@ -7,32 +7,12 @@ import configparser
 from os import path
 import syslog
 
-
 """
 Default options
 """
-#TODO: more default options...
 _CONFIG_DEFAULTS = {
-      "general": {
-          "poll_interval": 10,
-          "averaging_time": 5,
-      },
-
-      "calibration" : {
-        "max_ncups" : 10.0,
-        "coffee_full_value" : 1024,
-        "coffee_empty_decanter_value" : 100,
-      },
-
-      "database" : {
-        "dbname": "kahvidb",
-        "range_query_max_items": 1000,
-      },
-
       "telegram" : {
         "bot_token" : "",
-        "plot_length" : 30.0,
-        "data_unavailable_threshold" : 30.0,
         "group_trigger_threshold" : 20,
       },
 
@@ -48,7 +28,7 @@ def get_config_dict(filename = None):
     cfg_path = path.dirname(__file__)
     filename = path.join(cfg_path, "config.ini")
 
-  cp = configparser.ConfigParser() #_CONFIG_DEFAULTS)
+  cp = configparser.ConfigParser()
 
   # read default values from dict if they are not given in the config file.
   cp.read_dict(_CONFIG_DEFAULTS)
@@ -60,12 +40,10 @@ def get_config_dict(filename = None):
   syslog.syslog(syslog.LOG_INFO, "config: Using configuration file " + filename)
 
   # check for placeholder telegram bot token
-  if all(map(lambda x: x == "X", cp["telegram"]["bot_token"])):
+  if set(cp["telegram"]["bot_token"]) == set("X"):
     cp["telegram"]["bot_token"] = ""
 
   return cp
-
-  #def __getitem__(self, i): self.configparser.
 
 
 if __name__ == "__main__":
