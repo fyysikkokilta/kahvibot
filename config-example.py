a6d3ae8b27f6e97e7e5b4388a20836f25953c26d
Marton Gunyho <andras.gunyho@aalto.fi>
2022-03-05 20:52:21 +0200

    Add example config file

---
+++ config-example.py
@@ -0,0 +1,18 @@
+"""
+Minimal config file for kahvibot. Just define values as normal Python code.
+"""
+
+# put your bot token here as a string
+bot_token = ""
+
+# the tg username of the bot's admin.
+admin_username = ""
+
+# if a message contains any of these words, the bot responds
+trigger_words = [
+    "kahvi",
+    "\u2615", # coffee emoji
+    "tsufe",
+    "kahavi",
+    #"sima", # wappu mode
+]
